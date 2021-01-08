4d9e56c10d01b95413d8f726c267b934d8d95d2a
Prince Fefar <pfefar1@gmail.com>
2021-01-08 02:35:12 -0500

    #scrabble-score# solution added

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,2 +1,36 @@
 def score(word):
-    pass
+
+    set1 = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]
+    set2 = ["D", "G"]
+    set3 = ["B", "C", "M", "P"]
+    set4 = ["F", "H", "V", "W", "Y"]
+    set5 = ["K"]
+    set6 = ["J", "X"]
+    set7 = ["Q", "Z"]
+
+    score = 0
+    clean_word = word.upper()
+
+    for s in clean_word:
+        if s in set1:
+            score += 1
+
+        if s in set2:
+            score += 2
+
+        if s in set3:
+            score += 3
+
+        if s in set4:
+            score += 4
+
+        if s in set5:
+            score += 5
+
+        if s in set6:
+            score += 8
+
+        if s in set7:
+            score += 10
+
+    return score
--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,15 @@
+from collections import defaultdict
+
+
 def count_words(sentence):
-    pass
+    words = sentence.lower().split(" ")
+
+    dict = defaultdict(lambda: 0)
+
+    for word in words:
+        dict[word] += 1
+
+    return dict
+
+
+print(count_words("one of each"))
