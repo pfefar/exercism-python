67a275217fa10fba5df8cb924fbea6a5d9faff47
Prince Fefar <pfefar1@gmail.com>
2021-01-08 02:07:52 -0500

    #isogram# solution added

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,12 @@
 def is_isogram(string):
-    pass
+
+    letter_list = []
+    clean_str = string.lower()
+
+    for s in clean_str:
+        if s.isalpha():
+            if s in letter_list:
+                return False
+        letter_list.append(s)
+
+    return True
