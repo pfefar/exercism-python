9996411b3890fdeebf679e40567cb084ea3d40f4
Prince Fefar <pfefar1@gmail.com>
2021-01-08 01:44:03 -0500

    #hamming# solution added

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,21 @@
 def distance(strand_a, strand_b):
-    pass
+    stra = strand_a
+    strb = strand_b
+
+    if len(stra) != len(strb):
+        raise ValueError("Length not same")
+
+    count = 0
+
+    for i in range(len(stra)):
+
+        if stra[i] != strb[i]:
+            count += 1
+
+    return count
+
+
+# -----------  Pythonic Code --------------
+
+# def distance(strand1, strand2):
+#     return sum(i != j for i, j in zip(strand1, strand2))
