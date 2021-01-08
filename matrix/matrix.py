b301699b743aba64c60848e4ff1a3f6ac086b0bb
Prince Fefar <pfefar1@gmail.com>
2021-01-08 01:04:13 -0500

    #matrix# solution added

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,9 +1,32 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        self.matrix = matrix_string.split("\n")
+
+        self.final = []
+
+        for i in self.matrix:
+            arr2 = []
+            l = i.split(" ")
+            for j in l:
+                arr2.append(int(j))
+            self.final.append(arr2)
 
     def row(self, index):
-        pass
+        return self.final[index - 1]
 
     def column(self, index):
-        pass
+        t = [*zip(*self.final)]
+        return list(t[index - 1])
+
+
+# ----- Pythonic Solution --------------
+
+# class Matrix:
+#     def __init__(self, matrix_string):
+#         self.matrix = [[int(item) for item in row.split(' ')] for row in matrix_string.split('\n')]
+
+#     def row(self, index):
+#         return self.matrix[index-1]
+
+#     def column(self, index):
+#         return list(list(zip(*self.matrix))[index-1])
